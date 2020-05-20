732c896e93e1b5c0c5f5965827dca095aacf7a63
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-20 12:40:59 +0000

    //commit

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const http=require("http")
-const fs=require('fs')
+const http = require("http");
 
-const server=http.createServer((req,res)=>{
-  
-  const url=req.url
-  const method=req.method
-  if(url==='/'){
-    res.write('<html>')
-  res.write('<head><title>Enter Message</title></head>')
-  res.write('<body><form action="/message" method="POST"><input type="text" name="measage"><button type="submit">Send</button></form></body>')
-  res.write('</html>')
-  return res.end()
-  }
-  if(url==='/message' && method==='POST'){
-    req.on('data',(chunk)=>{
-      const body =[]
-      console.log(chunk)
-      body.push(chunk)
-    })
-    req.on('end',()=>{
-      const parseBody=Buffer.concat(body).toString()
-      console.log(parseBody)
-    })
-     fs.writeFileSync('hello.txt','DUmmmay')
-     res.statusCode=302
-     res.setHeader('Location','/')
-     return res.end()
-  }
-  res.setHeader('Content-type','text/html')
-  //console.log(req.url,req.method,req.header)
-  //process.exit()
-  res.write('<html>')
-  res.write('<head><title>My first page</title></head>')
-  res.write('<body><h1>Hi I am Riyaz</h1></body>')
-  res.write('</html>')
-  res.end()
-
-  })
-server.listen(3000)
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.write("<h1>Welcome to kongu engineering college</h1>")
+   response.end();
+}).listen(8081)
